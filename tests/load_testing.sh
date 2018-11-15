0bfc15ff896c2c518e4ee4d2061b598fb4a12c28
mijuanlo <m.angel.juan@gmail.com>
2018-11-15 03:33:14 +0100

    Add mysql password

--- tests/load_testing.sh
+++ tests/load_testing.sh
@@ -4,7 +4,7 @@
 i=0
 
 # Create table
-mysql -u app_user  -h 127.0.0.1  -P6033  -Bse "create table if not exists my_database.t(id int)"
+mysql -u app_user -proot -h 127.0.0.1  -P6033  -Bse "create table if not exists my_database.t(id int)"
 
 # Writting 
-while true; do let i++; echo "INSERTING ${i}";  mysql -u app_user  -h 127.0.0.1  -P6033  -Bse "insert into my_database.t(id) values (current_timestamp(5))"; done
+while true; do let i++; echo "INSERTING ${i}";  mysql -u app_user -proot -h 127.0.0.1  -P6033  -Bse "insert into my_database.t(id) values (current_timestamp(5))"; done
